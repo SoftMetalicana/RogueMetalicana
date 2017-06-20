a405f007ba103cfc2e3d08fb06c8d8e29309773e
nikolayoyo <nikola.yy@icloud.com>
2017-06-20 17:23:29 +0300

    ADS Shitt

---
+++ RogueMetalicana/RogueMetalicana/Constants/Visualisator/VisualisatorConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueMetalicana.Constants.Visualisator
+{
+    class VisualisatorConstants
+    {
+        public const string LegendEnemyHeading = "\nSymbol    Meaning             Difficulty" +
+                                                 "\n----------------------------------------------------------------------------------";
+
+        public const string LegendObstaclesHeading = "\nSymbol    Meaning             Description" +
+                                                 "\n----------------------------------------------------------------------------------";
+
+        public const string LegendPlacesHeading = "\nSymbol    Meaning" +
+                                                     "\n----------------------------------------------------------------------------------";
+
+        public const string SeparatorLine = "----------------------------------------------------------------------------------";
+
+        public const string PlaceGainConsumed = "Congratulations you have just gained from this ";
+
+        public const string PlaceAlreadyVisited = "Dude, you have already visited this place! Nothing to consume!";
+
+        public const string UnableToCompleteLevel = "Don't be cheaty! You must get all enemies killed first!";
+    }
+}
